90d02440e24cb9d64983e60141acb0fe8bb05f19
luysla <dyana.tavares1@gmail.com>
2020-09-17 08:27:09 -0300

    Adicionei pasta para os componentes do Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="navbar primary__navbar justify-content-center">
+      <img class="primary__navbar__logo" src="../assets/imgs/logo.png" alt="Logo do sistema">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
